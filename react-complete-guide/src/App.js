0d0d128c5fbf26bbd7a0eb52a3f4b5b4c58d54af
Gabriele Luz <gabrieleluz@infojr.com.br>
2018-12-31 19:13:00 -0300

    adds `deletePersonHandler`

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -15,14 +15,12 @@ class App extends Component {
         showPersons: false,
     }
 
-    switchNameHandler = (newName) => {
-        this.setState({
-            persons: [
-            { name : newName, age: 28 },
-            { name : "Gabriele", age: 20 },
-            { name : "Stephanie", age: 27 }
-            ]
-        })
+
+    deletePersonHandler = (personsIndex) => {
+        //const persons = this.state.persons.slice();
+        const persons = [...this.state.persons];
+        persons.splice(personsIndex, 1);
+        this.setState({persons: persons});
     }
 
     nameChangeHandler = (event) => {
@@ -56,20 +54,12 @@ class App extends Component {
 
             persons = (
                     <div>
-                        <Person
-                            name={this.state.persons[0].name}
-                            age={this.state.persons[0].age} />
-
-                        <Person
-                            name={this.state.persons[1].name}
-                            age={this.state.persons[1].age}
-                            click={this.switchNameHandler.bind(this, "Max!")}> My Hobbies: Racing </Person>
-
-                        <Person
-                            name={this.state.persons[2].name}
-                            age={this.state.persons[2].age}
-                            changed={this.nameChangeHandler}
-                        />
+                        {this.state.persons.map((person, index) => {
+                            return <Person
+                                click={() => this.deletePersonHandler(index)}
+                                name={person.name}
+                                age={person.age} />
+                        })}
                     </div>
                     )
         }
