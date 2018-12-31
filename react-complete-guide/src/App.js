09c059cf8a390fb10ede1e86542d5b5eb917d05d
Gabriele Luz <gabrieleluz@infojr.com.br>
2018-12-31 18:42:48 -0300

    updates conditional content display

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -51,6 +51,29 @@ class App extends Component {
             borderRadius: "8px"
         };
 
+        let persons = null;
+        if(this.state.showPersons) {
+
+            persons = (
+                    <div>
+                        <Person
+                            name={this.state.persons[0].name}
+                            age={this.state.persons[0].age} />
+
+                        <Person
+                            name={this.state.persons[1].name}
+                            age={this.state.persons[1].age}
+                            click={this.switchNameHandler.bind(this, "Max!")}> My Hobbies: Racing </Person>
+
+                        <Person
+                            name={this.state.persons[2].name}
+                            age={this.state.persons[2].age}
+                            changed={this.nameChangeHandler}
+                        />
+                    </div>
+                    )
+        }
+
         return (
                 <div className="App">
                     <h1>Hi, I'm a react app</h1>
@@ -60,31 +83,13 @@ class App extends Component {
                         style={style}
                         onClick={this.togglePersonsHandler}>Switch name</button>
 
-                    {
-                        (this.state.showPersons) ?
-                            <div>
-                                <Person
-                                    name={this.state.persons[0].name}
-                                    age={this.state.persons[0].age} />
+                    {persons}
 
-                                <Person
-                                    name={this.state.persons[1].name}
-                                    age={this.state.persons[1].age}
-                                    click={this.switchNameHandler.bind(this, "Max!")}> My Hobbies: Racing </Person>
+                </div>
 
-                                <Person
-                                    name={this.state.persons[2].name}
-                                    age={this.state.persons[2].age}
-                                    changed={this.nameChangeHandler}
-                               />
-                            </div> : null
-                    }
+                );
 
-            </div>
-
-            );
-
-            //return React.createElement('div', {className: 'App' }, React.createElement('h1', null,  "Does this work now?"));
+        //return React.createElement('div', {className: 'App' }, React.createElement('h1', null,  "Does this work now?"));
     }
 }
 
