62c27380f8667faccc2fbf39100fc3c69405f626
Gabriele Luz <gabrieleluz@infojr.com.br>
2019-01-07 00:52:09 -0300

    using error boundaries

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Person from './Person/Person';
+import ErrorBoundary from './ErrorBoundary/ErrorBoundary';
 import classes from './App.module.css';
 
 class App extends Component {
@@ -53,12 +54,13 @@ class App extends Component {
             persons = (
                     <div>
                         {this.state.persons.map((person, index) => {
-                        return <Person
+                        return <ErrorBoundary
+                            key={person.id}
+                            > <Person
                             click={() => this.deletePersonHandler(index)}
                             name={person.name}
                             age={person.age}
-                            key={person.id}
-                            changed={(event) => this.nameChangeHandler(event, person.id)}/>
+                            changed={(event) => this.nameChangeHandler(event, person.id)}/> </ErrorBoundary>
                         })}
                     </div>
                     );
--- react-complete-guide/src/Person/Person.js
+++ react-complete-guide/src/Person/Person.js
@@ -3,6 +3,12 @@ import classes from './Person.module.css'
 
 const person = (props) => {
 
+    const rnd = Math.random();
+
+    if( rnd > 0.7 ) {
+        throw new Error("Something went wrong");
+    }
+
     return (
         <div className={classes.Person} >
             <p onClick={props.click}>I'm {props.name} and I am {props.age}  years old!</p>
