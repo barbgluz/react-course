bde51a3d19d4e644a5828f4f4afdbc50f157e169
Gabriele Luz <gabrieleluz@infojr.com.br>
2019-01-11 10:52:56 -0300

    updating lifecycle hooks

--- react-complete-guide/src/containers/App.js
+++ react-complete-guide/src/containers/App.js
@@ -28,6 +28,19 @@ class App extends Component {
         console.log('[App.js] Inside componentDidMount');
     }
 
+    shouldComponentUpdate(nextProps, nextState) {
+        console.log('[UPDATE App.js] Inside shouldComponentUpdate', nextProps, nextState);
+        return true;
+    }
+
+    componentWillUpdate(nextProps, nextState) {
+        console.log('[UPDATE App.js] Inside componentWillUpdate', nextProps, nextState);
+    }
+
+    componentDidUpdate() {
+        console.log('[UPDATE App.js] Inside componentDidUpdate');
+    }
+
     deletePersonHandler = (personsIndex) => {
         //const persons = this.state.persons.slice();
         const persons = [...this.state.persons];
